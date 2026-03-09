a11b67d232f39c8cd4df37d42a6c476aac4b2585
Jason <jason13.deng@gmail.com>
2026-03-09 16:50:40 +0900

    feat: finish addGame in queries.js

--- db/queries.js
+++ db/queries.js
@@ -8,34 +8,61 @@ async function addGame(
 	genres,
 	platforms,
 ) {
-    // insert game
-	await pool.query(
+	// insert game
+	const { rows } = await pool.query(
 		`
         INSERT INTO games (title, release_year, price)
-        VALUES ($1, $2, $3) ON CONFLICT DO NOTHING
+        VALUES ($1, $2, $3)
+        RETURNING id
         `,
 		[title, release_year, price],
 	);
 
-    // insert genres
-	await Promise.all(genres.map((genre) => {
-		return pool.query(`
+	const gameId = rows[0].id;
+
+	// insert genres
+	await Promise.all(
+		genres.map(async (genre) => {
+			const { rows } = await pool.query(
+				`
             INSERT INTO genres (name)
-            VALUES ($1) ON CONFLICT DO NOTHING
-            `, [genre]);
-	}));
+            VALUES ($1)
+            RETURNING id
+            `,
+				[genre],
+			);
+			const genreId = rows[0].id;
 
-    // insert platforms
-    await Promise.all(platforms.map((platform) => {
-		return pool.query(`
+			await pool.query(
+				`
+            INSERT INTO game_genre (game_id, genre_id)
+            VALUES ($1, $2)   
+            `,
+				[gameId, genreId],
+			);
+		}),
+	);
+
+	// insert platforms
+	await Promise.all(
+		platforms.map(async (platform) => {
+			const { rows } = await pool.query(
+				`
             INSERT INTO platforms (name)
-            VALUES ($1) ON CONFLICT DO NOTHING
-            `, [platform]);
-	}));
+            VALUES ($1)
+            RETURNING id
+            `,
+				[platform],
+			);
+			const platformId = rows[0].id;
 
-    const game_id = await pool.query(`
-        SELECT id FROM games 
-        WHERE title = $1
-        `, [title])
-
+			await pool.query(
+				`
+            INSERT INTO game_platform (game_id, platform_id)
+            VALUES ($1, $2)   
+            `,
+				[gameId, platformId],
+			);
+		}),
+	);
 }
