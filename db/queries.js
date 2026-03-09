3d643fe266b30613f0bd43703e9404d72cd492a0
Jason <jason13.deng@gmail.com>
2026-03-09 15:36:59 +0900

    fix: make game, genre, platform names unique in db

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,7 @@
 const pool = require("./pool");
+
+async function addGame(title, release_year, price, genres, platforms){
+    // check if game exists
+        // if not then add
+        // if exists then check if release_
+}
