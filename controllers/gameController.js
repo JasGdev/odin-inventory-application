3e32af98e3ac8f0c9a127c276277e6160123617f
Jason <jason13.deng@gmail.com>
2026-03-09 17:48:37 +0900

    feat: Setup navigation, initial addGame

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -4,3 +4,18 @@ exports.gamesGet = async (req, res) => {
     const games = await db.getAllGames()
     res.render('games/games', )
 }
+
+exports.newGameGet = (req, res) => {
+    db.getAllGenres()
+
+    res.render('games/newGame')
+}
+
+exports.newGamePost = (req, res) => {
+    const title = req.body.title;
+    const release_year = req.body.release_year;
+    const price = req.body.price;
+    db.addGame()
+    console.log(title + release_year + price)
+    res.redirect('/')
+}
--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -3,5 +3,7 @@ const gameRouter = Router();
 const gameController = require("../controllers/gameController");
 
 gameRouter.get('/', gameController.gamesGet)
+gameRouter.get('/new', gameController.newGameGet)
+gameRouter.post('/new', gameController.newGamePost)
 
 module.exports = gameRouter;
