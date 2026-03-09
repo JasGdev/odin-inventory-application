6a07cd37cd3e69de261e5379047ea5a3a05b51a9
Jason <jason13.deng@gmail.com>
2026-03-09 15:15:51 +0900

    feat: finish populateDB script

---
+++ db/populateDB.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const { Client } = require('pg');
+
+const SQL = `
+DROP TABLE IF EXISTS game_genres;
+DROP TABLE IF EXISTS game_platforms;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS platforms;
+
+CREATE TABLE IF NOT EXISTS games (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR ( 255 ) NOT NULL,
+    description TEXT NOT NULL,
+    release_year INT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS platforms (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS game_genre (
+    game_id INT,
+    genre_id INT,
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY (game_id) REFERENCES games(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+CREATE TABLE IF NOT EXISTS game_platform (
+    game_id INT,
+    platform_id INT,
+    PRIMARY KEY (game_id, platform_id),
+    FOREIGN KEY (game_id) REFERENCES games(id),
+    FOREIGN KEY (platform_id) REFERENCES platforms(id)
+);
+
+`;
+
+async function main() {
+	console.log('seeding...');
+	const client = new Client({
+		host: 'localhost',
+		user: process.env.USER,
+		database: process.env.DATABASE,
+		password: rocess.env.PASSWORD,
+		port: 5432,
+	});
+	await client.connect();
+	await client.query(SQL);
+	await client.end();
+	console.log('done');
+}
+
+main();
