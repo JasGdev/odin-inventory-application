0bfdf2dd1621a53ea6d538bd191293f69c3d196e
Jason <jason13.deng@gmail.com>
2026-03-10 14:27:21 +0900

    feat: move genres/games list to Home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,7 @@
-exports.indexGet = (req, res) => {
-    res.render('index')
+const db = require('../db/queries');
+
+exports.homePageGet = async (req, res) => {
+    const games = await db.getAllGames();
+    const genres = await db.getAllGenres();
+    res.render('index', {games, genres})
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
-indexRouter.get('/', indexController.indexGet)
+indexRouter.get('/', indexController.homePageGet)
 
 
 module.exports = indexRouter;
